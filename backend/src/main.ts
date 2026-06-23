6958791b05e2cde4ef2995113bb1180a449812f9
matibbaez <matiascaneda3@gmail.com>
2026-06-23 08:52:03 -0300

    deploy-prep: favicon inyectado, cors activado y puerto dinamico configurado

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,9 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  
+  // 1. ESCUDO CORS: Permite que Vercel (Front) hable con Render (Back)
   app.enableCors(); 
 
-  await app.listen(process.env.PORT ?? 3000);
+  // 2. PUERTO DINÁMICO: Render te va a asignar un puerto aleatorio, jamás usa el 3000
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
