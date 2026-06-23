0be82360e2908d5f4368723db1d945f02d54230a
matibbaez <matiascaneda3@gmail.com>
2026-06-23 09:07:24 -0300

    fix(cors): apertura de metodos OPTIONS y cabeceras en NestJS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,10 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  // 1. ESCUDO CORS: Permite que Vercel (Front) hable con Render (Back)
-  app.enableCors(); 
+  app.enableCors({
+    origin: '*', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
 
-  // 2. PUERTO DINÁMICO: Render te va a asignar un puerto aleatorio, jamás usa el 3000
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
